e69a4b4f40b8bd837ef9578532ecfdbb5d4c8edd
natashasok <51156572+natashasok@users.noreply.github.com>
2020-04-27 11:57:48 +0300

    Первый тест

--- selenium-test/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ selenium-test/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 public class MyFirstTest {
     private WebDriver driver;
     private WebDriverWait wait;
@@ -21,7 +23,9 @@ public class MyFirstTest {
    @Test
    public void myFirstTest() {
        driver.get("https://www.google.com/");
-       driver.findElements(By.name("q"));
+       driver.findElement(By.name("q")).sendKeys("webdriver");
+       driver.findElement(By.name("btnG")).click();
+       wait.until(titleIs("webdriver - Поиск в Google"));
    }
 
    @After
