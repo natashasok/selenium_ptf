e1cfec78ed20a1f8ac027f0eeff64c4d600f41f5
natashasok <51156572+natashasok@users.noreply.github.com>
2020-05-21 19:17:58 +0300

    Задание8_итог

--- selenium-test/src/test/java/ru/stqa/training/selenium/TestSticker.java
+++ selenium-test/src/test/java/ru/stqa/training/selenium/TestSticker.java
@@ -11,11 +11,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestSticker {
     private WebDriver driver;
     private WebDriverWait wait;
+
+    public boolean isElementPresent(WebDriver driver, By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
 
     @Before
     public void start(){
@@ -28,11 +40,11 @@ public class TestSticker {
     @Test
     public void testSticker() {
         driver.get("http://localhost/litecart/en/");
+
+
         List<WebElement> ducks = driver.findElements(By.cssSelector("li[class='product column shadow hover-light']"));
         for (WebElement e: ducks) {
-            //listLink.add(e.getAttribute("href"));
-            System.out.println(e.getAttribute("href"));
-
+            assertTrue(isElementPresent(driver,By.cssSelector("div[class*='sticker']")));
         }
     }
 
