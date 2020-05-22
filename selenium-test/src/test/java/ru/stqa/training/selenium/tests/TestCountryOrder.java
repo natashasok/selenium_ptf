3982afdefd869f070a805d786bf7dd136e3f7c9b
natashasok <51156572+natashasok@users.noreply.github.com>
2020-05-22 14:12:12 +0300

    Задание 9 1а

--- selenium-test/src/test/java/ru/stqa/training/selenium/tests/TestCountryOrder.java
+++ selenium-test/src/test/java/ru/stqa/training/selenium/tests/TestCountryOrder.java
@@ -13,6 +13,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestCountryOrder extends TestBase {
+
+    public boolean sort(ArrayList list) {
+        List sortedList = new ArrayList(list);
+        Collections.sort(sortedList);
+        boolean sorted = sortedList.equals(list);
+        return sorted;
+    }
+
     @Test
       public void testCountryOrder(){
         loginAdmin();
@@ -21,16 +29,20 @@ public class TestCountryOrder extends TestBase {
         List<String> country_zone = new ArrayList<>();
         List<WebElement> country = driver.findElements(By.cssSelector("tr[class=row] a"));
         for(WebElement c: country) {
+
             if (!c.getAttribute("title").equals("Edit")){
                country_name.add(c.getAttribute("textContent"));
             }
-            //if (){
+            //if (!c.getAttribute("title").equals("0")){
              //   country_zone.add(c.getAttribute("textContent"));
            // }
         }
-        ArrayList<String> sortedList = (ArrayList<String>) country_name;
-        Collections.sort(sortedList);
-        assertEquals(sortedList, country_name);
+
+
+        //List sortedList = new ArrayList(country_name);
+        //Collections.sort(sortedList);
+        assertTrue(sort((ArrayList) country_name));
+
 
     }
 
