dacf7f4a5b75aa8e7589d1cdfbc7df6fce4e07de
natashasok <51156572+natashasok@users.noreply.github.com>
2020-05-22 17:56:59 +0300

    Задание 9 1б

--- selenium-test/src/test/java/ru/stqa/training/selenium/tests/TestCountryOrder.java
+++ selenium-test/src/test/java/ru/stqa/training/selenium/tests/TestCountryOrder.java
@@ -27,22 +27,37 @@ public class TestCountryOrder extends TestBase {
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
         List<String> country_name = new ArrayList<>();
         List<String> country_zone = new ArrayList<>();
+        List<String> country_on_zone_name = new ArrayList<>();
         List<WebElement> country = driver.findElements(By.cssSelector("tr[class=row]"));
         for(WebElement c: country) {
-            //driver.findElement(By.xpath("//td/a[not(contains(@title, 'Edit'))]")).getAttribute("textContent");
-            //if (!c.getAttribute("title").equals("Edit")){
               country_name.add(c.findElement(By.xpath("./td/a[not(contains(@title, 'Edit'))]")).getAttribute("textContent"));
-           // }
+            if (!c.findElement(By.xpath("./td[6]")).getAttribute("textContent").equals("0")){
+                country_zone.add(c.findElement(By.xpath("./td/a[not(contains(@title, 'Edit'))]")).getAttribute("href"));
+            }
         }
-        assertTrue(sort((ArrayList) country_name));
-        for (int i=0; i<country_name.size(); i++){
-            System.out.println(country_name.get(i));
+
+            assertTrue(sort((ArrayList) country_name));
+        //for (int i=0; i<country_zone.size(); i++){
+         //  System.out.println(country_zone.get(i));
+      //  }
+
+        for(String z: country_zone){
+            //System.out.println(driver.findElement(By.xpath("//table[contains(@class,'dataTable')]//a[not(contains(@title, 'Edit')) and contains(@href, '"+z+"')]")).getAttribute("textContent"));
+            //driver.findElement(By.xpath("//table[contains(@class,'dataTable')]//a[not(contains(@title, 'Edit')) and contains(@href, '"+z+"')]")).click();
+            driver.get(""+z+"");
+            List<WebElement> country_on_zone = driver.findElements(By.xpath("//table[contains(@id,'table-zones')]//td[3]"));
+            for(WebElement c: country_on_zone){
+                country_on_zone_name.add(c.getAttribute("textContent"));
+                System.out.println(c.getAttribute("textContent"));
+            }
+            assertTrue(sort((ArrayList) country_on_zone_name));
         }
        // for(WebElement c: country) {
       //      driver.findElements(By.cssSelector(""));
       //  }
         //row[not(contains(title,'Edit'))]
         // $x ("//tr[contains(@class,'row')]/td/a[not(contains(@title, 'Edit'))]")
+        // $x ("//table[contains(@id,'table-zones')]//td[3]")
 
     }
 
