676a2f6d7cf54a7658daea829224a58276ae99ce
natashasok <51156572+natashasok@users.noreply.github.com>
2020-05-25 13:25:11 +0300

    10 цвета

--- selenium-test/src/test/java/ru/stqa/training/selenium/tests/TestProductSelection.java
+++ selenium-test/src/test/java/ru/stqa/training/selenium/tests/TestProductSelection.java
@@ -17,12 +17,17 @@ public class TestProductSelection extends TestBase{
         driver.get("http://localhost/litecart/en/");
        ProductData onFirstPage = new ProductData().withTitle(driver.findElement(By.cssSelector("[id=box-campaigns] a.link div.name")).getAttribute("textContent"))
                         .withRegularPrice(driver.findElement(By.cssSelector("[id=box-campaigns] a.link .regular-price")).getAttribute("textContent"))
-        .withCampaignPrice(driver.findElement(By.cssSelector("[id=box-campaigns] a.link .campaign-price")).getAttribute("textContent"));
+                        .withCampaignPrice(driver.findElement(By.cssSelector("[id=box-campaigns] a.link .campaign-price")).getAttribute("textContent"));
+        driver.findElement(By.cssSelector("[id=box-campaigns] a.link .regular-price")).getCssValue("color")
+                .equals("rgba(119, 119, 119, 1)");
+        driver.findElement(By.cssSelector("[id=box-campaigns] a.link .campaign-price")).getCssValue("color")
+                .equals("rgba(204, 0, 0, 1)");
         driver.findElement(By.cssSelector("[id=box-campaigns] a.link")).click();
         wait = new WebDriverWait(driver,100);
         ProductData onSecondPage =new ProductData().withTitle(driver.findElement(By.cssSelector("[id=box-product] h1.title")).getAttribute("textContent"))
         .withRegularPrice(driver.findElement(By.cssSelector("[id=box-product] s.regular-price")).getAttribute("textContent"))
         .withCampaignPrice(driver.findElement(By.cssSelector("[id=box-product] strong.campaign-price")).getAttribute("textContent"));
         assertTrue(onFirstPage.equals(onSecondPage));
+
     }
 }
